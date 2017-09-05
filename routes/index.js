882b8fee0c9f5382156e3985f3e20d7f0ad7694d
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-09-05 18:24:30 -0500

    adding hex names and terrain

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const types = ['Intelligent', 'Elemental', 'Beast', 'Fuegonauts', 'Night Axe'];
+const where = ['Light Jungle', 'Heavy Jungle', 'Mtns Jungle', 'Volcano', 'Volcanic', 'Ruins', 'Village'];
+const beast = {
+  LightJungle: ['Crystal Frog', 'Flayfiend', 'Duecadre', 'Muttering Serp','Broadback', 'Singing Golem', 'Tabibari', 'Giant Centipede', 'Boar', 'Giant Rat', 'Zip Bird', 'Vyderac', 'Blindfire Vine', 'Dire Boar', 'Spine Dragon', 'Poison Dart Frog'],
+  HeavyJungle: ['Poison Dart Frog', 'Obsidian Digger', 'Blindfire Carpet']
+}
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
